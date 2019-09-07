fd4fb17483c297077b2155f49d1a65a3877b63a4
Bruno Sendras <brunosendras@gmail.com>
2019-09-06 21:50:10 -0300

    Modificado script build.bat

--- build.bat
+++ build.bat
@@ -1,14 +1,17 @@
+cd src
 c:\GnuWin32\bin\flex Lexico.l
 pause
 c:\GnuWin32\bin\bison -dyv Sintactico.y
 pause
-c:\MinGW\bin\gcc.exe lex.yy.c y.tab.c -o TPFinal.exe
+c:\MinGW\bin\gcc.exe lex.yy.c y.tab.c -o ..\bin\Primera.exe
 pause
 pause
-TPfinal.exe Prueba.txt
+cd ..
+bin\Primera.exe test\Prueba.txt
+
+cd src
 del lex.yy.c
 del y.tab.c
 del y.output
 del y.tab.h
-del TPFinal.exe
 pause
