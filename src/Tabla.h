70caac7c96e1f904d8c9588f722f77928b5809f3
Fede Amoruso <sergioamoruso91@gmail.com>
2019-10-06 15:59:59 -0300

    Implementar correcciones tabla de simbolos
    * Se modificaron los define para identificar a las constantes de manera
      tal que tengan como prefijo "Const"
    * Se modifico el campo "longitud" de la estructura de registro de la
      tabla de simbolos de int a char[] para poder asignarle vacio si se
      trata de una variable
    * Se actualizaron define de formato y longitud de registro de tabla de
      simbolos para asignar el minimo espacio necesario para cada columna
    * Se modifico archivo "Prueba.txt" para quitar las constantes numericas
      negativas

--- src/Tabla.h
+++ src/Tabla.h
@@ -1,16 +1,16 @@
 #ifndef TABLA_H
 #define TABLA_H
 
-#define TD_CTE_S "String"
-#define TD_CTE_I "Integer"
-#define TD_CTE_F "Float"
+#define TD_CTE_S "ConstString"
+#define TD_CTE_I "ConstInteger"
+#define TD_CTE_F "ConstFloat"
 #define TD_UNDEFINED "Undefined"
 
 typedef struct simbolo_s {
    char nombre[50];
    char tipo[20];
    char valor[32];
-   int longitud;
+   char longitud[3];
 } simbolo_t;
 
 #define MAX_SIM 200
